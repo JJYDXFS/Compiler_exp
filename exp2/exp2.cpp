3d1fe1fdb00f2eee0cda61074ab671888bb90a5e
Yuki <Yuki@DESKTOP-RS3GFIM>
2020-10-01 23:07:42 +0800

    Precede函数

--- exp2/exp2.cpp
+++ exp2/exp2.cpp
@@ -1,1 +1,54 @@
-﻿
+﻿char Precede(char a, char b)
+{
+	//判断符号优先级的函数
+	switch (a)
+	{
+		case'+':
+		{
+			if (b == '+' || b == '-' || b == ')' || b == '#')
+				return '>';
+			else return '<';
+			break;
+		}
+		case'-':
+		{
+			if (b == '+' || b == '-' || b == ')' || b == '#')
+				return '>';
+			else return '<';
+			break;
+		}
+		case'*':
+		{
+			if (b == '(')
+				return '<';
+			else return '>';
+			break;
+		}
+		case'/':
+		{
+			if (b == '(')
+				return '<';
+			else return '>';
+			break;
+		}
+		case'(':
+		{
+			if (b == ')')
+				return '=';
+			else return '<';
+			break;
+		}
+		case')':
+		{
+			return '>';
+			break;
+		}
+		case'#':
+		{
+			if (b == '#')
+				return '=';
+			else return '<';
+			break;
+		}
+	}
+}									
