ae33be8470c58444d10f0760cf18592aa3caf3e3
snjor-kii <63648690+snjor-kii@users.noreply.github.com>
2020-10-02 09:28:44 +0800

    Update exp2.cpp

--- exp2/exp2.cpp
+++ exp2/exp2.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 char Precede(char a, char b);
-int main(){
-	char a,b;
-	while(1){
-		cin>>a>>b;
-		cout<<Precede(a,b)<<endl;
+int main() {
+	char a, b;
+	while (1) {
+		cin >> a >> b;
+		cout << Precede(a, b) << endl;
 	}
-    return 0;
+	return 0;
 }
 
 char Precede(char a, char b)
@@ -16,40 +16,40 @@ char Precede(char a, char b)
 	//判断符号优先级的函数
 	switch (a)
 	{
-		case'|':
-		{
-			if (b == '(' || b == '*' ||  b == '.')
-				return '<';
-			else return '>';
-			break;
-		}
-		case'.':
-		{
-			if (b == ')' || b == '|' || b == '.' )
-				return '>';
-			else return '<';
-			break;
-		}
-		case'*':
-		{
-			if (b == '）' || b == '|' || b == '.'||b=='*')
-				return '>';
-			else return '<';
-			break;
-		}
-		case'(':
-		{
-			if (b == ')')
-				return '=';
-			else return '<';
-			break;
-		}
-		case')':
-		{
-			if (b = '(')
-				return '!';
-			else return '>';
-			break;
-		}
+	case'|':
+	{
+		if (b == '(' || b == '*' || b == '.')
+			return '<';
+		else return '>';
+		break;
 	}
-}									
+	case'.':
+	{
+		if (b == ')' || b == '|' || b == '.')
+			return '>';
+		else return '<';
+		break;
+	}
+	case'*':
+	{
+		if (b == ')' || b == '|' || b == '.' || b == '*')
+			return '>';
+		else return '<';
+		break;
+	}
+	case'(':
+	{
+		if (b == ')')
+			return '=';
+		else return '<';
+		break;
+	}
+	case')':
+	{
+		if (b == '(')
+			return '!';
+		else return '>';
+		break;
+	}
+	}
+}
