fea69cb455998feb8ebbe8ea6db06e46ceaf09b9
JJYDXFS <1043804998@qq.com>
2020-10-09 16:24:36 +0800

    加入闭包化简

--- exp2/checkInputPlus.cpp
+++ exp2/checkInputPlus.cpp
@@ -6,7 +6,7 @@ using namespace std;
 bool checkLegal(string check_string);	// 检验正则式是否合法
 bool checkBrackets(string check_string);	// 检验括号匹配及输入字符合法性
 bool checkOperator(string check_string);	// 检验运算符使用合法性
-string RemoveAddOp(string check_string);	// 去掉多余的连接运算符
+string ProcessOp(string check_string);	// 去掉多余的连接运算符、合并闭包 
 bool IsAlphameric(char c);	// 判断是否字母数字
 
 bool IsOp(char c) {
@@ -30,15 +30,14 @@ int main() {
 		e.clear();
 		cin >> e;
 	} while (!checkLegal(e));
-	e = RemoveAddOp(e);
+	e = ProcessOp(e);
 	cout << "输入正确！" << endl;
-	cout << "化简后的正规式：" << e <<endl;
+	cout << "化简后的正规式：" << e << endl;
 
 	return 0;
 }
 
-bool checkLegal(string check){	// 检验正则式是否合法
-
+bool checkLegal(string check) {	// 检验正则式是否合法
 	// 1、2一次遍历；3一次遍历；4一次遍历；目前共需要遍历三次
 	// 0. 首先不考虑转义
 	// 1. 检查输入字符合法性（字母数字+合法运算符），放在2中一起进行
@@ -48,14 +47,14 @@ bool checkLegal(string check){	// 检验正则式是否合法
 	return (checkBrackets(check) && checkOperator(check));
 }
 
-bool checkBrackets(string check){	// 检验括号匹配及输入字符合法性
+bool checkBrackets(string check) {	// 检验括号匹配及输入字符合法性
 	stack<int> STACK;	// 括号栈
 	for (int i = 0; i < check.length(); i++)
 	{
 		if (!IsAlphameric(check[i]) && !IsOp(check[i]))
 			return false;	// 出现非法字符
 		if (check[i] == '(') STACK.push(i);	// 左括号入栈
-		else if (check[i] == ')'){
+		else if (check[i] == ')') {
 			if (STACK.empty()) return false;	// 有右无左
 			else if (check[i - 1] == '(') return false;	// 空括号
 			else STACK.pop();	// 弹出左括号
@@ -73,7 +72,7 @@ bool checkOperator(string check) {
 	3. 单目运算符不能出现在双目运算符右侧【与4等价】
 	4. 双目运算符不能出现在单目运算符右侧
 	5. 双目运算符的两侧均不能出现双目运算符
-	6. 双目运算符的左侧无左括号，右侧无右括号 
+	6. 双目运算符的左侧无左括号，右侧无右括号
 	*/
 	if (IsUnaryOp(check[0]) || IsBinocularOp(check[0]))return false;	// 单目或双目运算符均不能出现在开头
 	if (IsBinocularOp(check[check.length() - 1]))return false;	// 所有双目运算符不能出现在结尾
@@ -87,18 +86,21 @@ bool checkOperator(string check) {
 	}
 	return true;
 }
-string RemoveAddOp(string check) {
+string ProcessOp(string check) {
 	// 去掉多余的连接运算符.
 	string e;
-	for (int i = 0; i < check.length(); i++)
-		if (check[i] != '.') 
+	for (int i = 0; i < check.length(); i++) {
+		if (check[i] != '.')
 			e.push_back(check[i]);
+		if (check[i] == '*' && check[i - 1] == '*')
+			continue;
+	}
 	return e;
 }
 
-bool IsAlphameric(char c){	// 判断是否字母数字
-	if (c >= 'a' && c <= 'z' || c >= 'A'&& c<= 'Z' 
-		|| c>='0' && c<='9')
+bool IsAlphameric(char c) {	// 判断是否字母数字
+	if (c >= 'a' && c <= 'z' || c >= 'A'&& c <= 'Z'
+		|| c >= '0' && c <= '9')
 		return true;
 	return false;
 }
